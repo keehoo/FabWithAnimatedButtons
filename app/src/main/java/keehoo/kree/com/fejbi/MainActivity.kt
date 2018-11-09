6fb12b5430bb539549ea02c66dd3564d29d13043
Krzysztof Kubicki <kkubicki@jitsolutions.pl>
2018-11-09 14:15:50 +0100

    Animation of one single button added with list

--- app/src/main/java/keehoo/kree/com/fejbi/MainActivity.kt
+++ app/src/main/java/keehoo/kree/com/fejbi/MainActivity.kt
@@ -1,7 +1,9 @@
 package keehoo.kree.com.fejbi
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.fab
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val map: HashMap<Int, () -> Unit> = HashMap()
 
+        map[R.drawable.ic_person_black_24dp] = { Toast.makeText(this, "asd", Toast.LENGTH_LONG).show() }
+
+        fab.addButtons(map)
     }
 }
--- customcompoundfab/src/main/java/keehoo/kree/com/customcompoundfab/CustomFab.kt
+++ customcompoundfab/src/main/java/keehoo/kree/com/customcompoundfab/CustomFab.kt
@@ -1,30 +1,59 @@
 package keehoo.kree.com.customcompoundfab
 
+import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.content.Context
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.widget.LinearLayout
+import android.view.View
+import android.widget.Button
 import com.airbnb.lottie.LottieAnimationView
-import keehoo.kree.com.customcompoundfab.R.id.animation_view
+
 
 class CustomFab @JvmOverloads constructor(
         context: Context,
         attrs: AttributeSet? = null,
-        defStyle: Int = 0,
-        defStyleRes: Int = 0) : LinearLayout(context, attrs, defStyle, defStyleRes) {
+        defStyle: Int = 0) : ConstraintLayout(context, attrs, defStyle) {
 
-    init {
-        LayoutInflater.from(context).inflate(R.layout.lottie_stuff, this, true)
-        val lottie: LottieAnimationView = findViewById(R.id.animation_view)
+    var buttons: MutableList<Button> = mutableListOf()
 
-        lottie.setOnClickListener {
-            onMenuOperation(it as LottieAnimationView, true)
+    var isCollapsed: Boolean = true
+        private set(value) {
+            field = value
+            animateFab()
+            animateButtons()
+
+        }
+
+    private fun animateButtons() {
+        buttons.forEach {
+            onAnimation(it, true)
         }
     }
 
-    fun onMenuOperation(lottieAnimationView: LottieAnimationView, menuExpanded: Boolean) {
-        val animator: ValueAnimator = if (menuExpanded) {
+    private val lottieAnimationView: LottieAnimationView
+        get() {
+            return findViewById(R.id.animation_view)
+        }
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.lottie_stuff, this, true)
+        val lottie: LottieAnimationView = lottieAnimationView
+
+        lottie.setOnClickListener {
+            animateFab()
+            fabFlipped()
+        }
+    }
+
+    private fun fabFlipped() {
+        isCollapsed = !isCollapsed
+    }
+
+    private fun animateFab() {
+        val animator: ValueAnimator = if (isCollapsed) {
             ValueAnimator.ofFloat(0.1F, 0.5F).setDuration(300)
         } else {
             ValueAnimator.ofFloat(0.6F, 1.0F).setDuration(300)
@@ -33,5 +62,66 @@ class CustomFab @JvmOverloads constructor(
             lottieAnimationView.progress = it.animatedValue as Float
         }
         animator.start()
+    }
+
+    fun onAnimation(view: View, shouldDisappear: Boolean) {
+
+        val measuredWidth = view.rootView.measuredWidth
+        val trueExtend = (-50F.div(100)).times(measuredWidth)
+
+        if (isCollapsed) {
+            val animation = ObjectAnimator.ofFloat(view, "translationX", trueExtend)
+            animation.duration = 300
+            animation.addUpdateListener {
+                when (it.currentPlayTime) {
+                    in 250..300 -> {
+                        view.translationZ = 100f
+                    }
+                    in 0..300 -> {
+                        if (view.visibility == View.INVISIBLE && shouldDisappear) view.visibility = View.VISIBLE
+                    }
+                }
+            }
+//            if (powerManagerService.isPowerSaveMode) view.translationZ = 100f //FIXME: Add instance of powerManagerService
+            view.visibility = View.VISIBLE
+            animation.start()
+        } else {
+            val animation = ObjectAnimator.ofFloat(view, "translationX", 0.0f)
+            view.translationZ = 0f
+            animation.duration = 300
+            animation.addUpdateListener {
+                when (it.currentPlayTime) {
+                    0L -> {
+                        view.translationZ = 0f
+                    }
+                    in 220..300 -> {
+                        if (view.visibility == View.VISIBLE && shouldDisappear) view.visibility = View.INVISIBLE
+                    }
+                }
+            }
+            animation.start()
+        }
+    }
+
+    fun addButtons(args: Map<Int, () -> Unit>) {
+
+        val layout = findViewById<ConstraintLayout>(R.id.main_fab_layout)
+        val set = ConstraintSet()
+
+        val view = Button(context)
+        view.id = View.generateViewId()
+        view.setCompoundDrawablesWithIntrinsicBounds(null, context.getDrawable(args.keys.first()), null, null)
+        view.setPadding(8,8,8,8)
+        view.setOnClickListener {
+            args.values.first()
+        }
+        layout.addView(view, 0)
+        set.clone(layout)
+        set.connect(view.id, ConstraintSet.END, layout.id, ConstraintSet.END, 8)
+        set.connect(view.id, ConstraintSet.BOTTOM, layout.id, ConstraintSet.BOTTOM, 28)
+        set.applyTo(layout)
+
+        buttons.add(view)
+
     }
 }
