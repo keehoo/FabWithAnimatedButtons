36369fda40d637ae4c5c23019a86a52c25772b8b
Krzysztof Kubicki <kkubicki@jitsolutions.pl>
2018-11-14 12:17:44 +0100

    Working

--- app/src/main/java/keehoo/kree/com/fejbi/MainActivity.kt
+++ app/src/main/java/keehoo/kree/com/fejbi/MainActivity.kt
@@ -13,24 +13,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val map: Map<Int, () -> Unit> = mapOf(
+        val drawableIdToLambda: Map<Int, () -> Unit> = mapOf(
                 R.drawable.ic_person_black_24dp to { logstuff("A") },
                 R.drawable.ic_filter to { logstuff("B") },
                 R.drawable.ic_location_24dp to { logstuff("C") }
+//                R.drawable.ic_whatshot_black_24dp to { logstuff("D") },
+//                R.drawable.ic_cake_black_24dp to { logstuff("E") },
+//                R.drawable.ic_child_care_black_24dp to { logstuff("F") }
         )
 
-        fab.addButtons(map
-                , FabParams(
-                40,
-                R.color.error_color_material_light,
-                16,
-                16
+        fab.setup(
+                drawableIdToLambda,
+                FabParams(
+                        buttonSize = 50,
+                        buttonaAvatarTintColor = R.color.error_color_material_light,
+                        buttonBackgroundColor = R.color.abc_color_highlight_material,
+                        buttonBottomOffset = 12,
+                        buttonEndOffset = 2,
+                        collapseAfterButtonClick = true,
+                        extractionOffset = 24
+                )
         )
-        )
+        fab.isPowerSaverMode = false
     }
 
     @SuppressLint("LogNotTimber")
-    private fun logstuff(s: String): Unit {
+    private fun logstuff(s: String) {
         Log.wtf("tag", s)
     }
 }
--- customcompoundfab/src/main/java/keehoo/kree/com/customcompoundfab/CustomFab.kt
+++ customcompoundfab/src/main/java/keehoo/kree/com/customcompoundfab/CustomFab.kt
@@ -4,7 +4,9 @@ import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.content.Context
 import android.content.res.Resources
+import android.graphics.Point
 import android.graphics.PorterDuff
+import android.graphics.drawable.GradientDrawable
 import android.support.annotation.ColorInt
 import android.support.annotation.ColorRes
 import android.support.constraint.ConstraintLayout
@@ -15,6 +17,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import com.airbnb.lottie.LottieAnimationView
+import android.view.WindowManager
 
 
 class CustomFab @JvmOverloads constructor(
@@ -22,21 +25,20 @@ class CustomFab @JvmOverloads constructor(
         attrs: AttributeSet? = null,
         defStyle: Int = 0) : ConstraintLayout(context, attrs, defStyle) {
 
-    var buttons: MutableList<Button> = mutableListOf()
+    private var buttons: MutableList<Button> = mutableListOf()
+    private lateinit var params: FabParams
 
-    var isExpanded: Boolean = false
+    private var isExpanded: Boolean = false
         private set(value) {
             field = value
             animateFab()
             animateButtons()
-
         }
 
-    private fun animateButtons() {
-        buttons.forEach {
-            onAnimation(it, true)
-        }
-    }
+    var isPowerSaverMode = false
+
+    private fun animateButtons() = buttons.forEach { onAnimation(it) }
+
 
     private val lottieAnimationView: LottieAnimationView
         get() {
@@ -70,14 +72,30 @@ class CustomFab @JvmOverloads constructor(
         animator.start()
     }
 
-    fun onAnimation(view: View, shouldDisappear: Boolean) {
+    fun onAnimation(view: View) {
+
+        val display = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        val p = Point()
+        display.defaultDisplay.getRealSize(p)
+        val isHorizontal = p.x > p.y
+        val viewPosition = Point(view.width, view.height)
+        val viewPadding = view.paddingEnd.toPx + view.paddingStart.toPx
+
+        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        val h = resources.getDimensionPixelSize(resourceId)
+
 
         val totalButtons = buttons.size.toFloat()
         val currentButton = buttons.indexOf(view as Button) + 1.toFloat()
-        val measuredWidth = view.rootView.measuredWidth
-        val modified = currentButton.div(totalButtons).div(75f).times(-1)
-        val trueExtend = modified.times(measuredWidth).times(60f)
+//        val measuredWidth = view.rootView.measuredWidth
+//        val modified = currentButton.div(totalButtons).div(75f).times(-1)
+//        val trueExtend = modified.times(measuredWidth).times(60f)
 
+        val trueExtend = (p.x.minus(viewPosition.x )
+
+                .removeBarHeight(isHorizontal, h)
+                .minus(params.buttonSize.toPx)
+                .times(currentButton.div(totalButtons)).times(-1))
         if (isExpanded) {
             val animation = ObjectAnimator.ofFloat(view, "translationX", trueExtend)
             animation.duration = 300
@@ -87,11 +105,11 @@ class CustomFab @JvmOverloads constructor(
                         view.translationZ = 100f
                     }
                     in 0..300 -> {
-                        if (view.visibility == View.INVISIBLE && shouldDisappear) view.visibility = View.VISIBLE
+                        if (view.visibility == View.INVISIBLE) view.visibility = View.VISIBLE
                     }
                 }
             }
-//            if (powerManagerService.isPowerSaveMode) view.translationZ = 100f //FIXME: Add instance of powerManagerService
+            if (isPowerSaverMode) view.translationZ = 100f
             view.visibility = View.VISIBLE
             animation.start()
         } else {
@@ -102,7 +120,7 @@ class CustomFab @JvmOverloads constructor(
                 when (it.currentPlayTime) {
                     0L -> view.translationZ = 0f
                     in 220..300 -> {
-                        if (view.visibility == View.VISIBLE && shouldDisappear) view.visibility = View.INVISIBLE
+                        if (view.visibility == View.VISIBLE) view.visibility = View.INVISIBLE
                     }
                 }
             }
@@ -110,42 +128,54 @@ class CustomFab @JvmOverloads constructor(
         }
     }
 
-    fun addButtons(args: Map<Int, () -> Unit>, params: FabParams = FabParams()) {
+    fun setup(args: Map<Int, () -> Unit>, params: FabParams = FabParams()) {
 
+        this.params = params
+        val layout = findViewById<ConstraintLayout>(R.id.main_fab_layout)
         var i = 0
+        buttons.clear()
         args.forEach { entry ->
 
-            val layout = findViewById<ConstraintLayout>(R.id.main_fab_layout)
             val set = ConstraintSet()
+            with(Button(context)) {
+                id = View.generateViewId()
+                layoutParams = ConstraintLayout.LayoutParams(
+                        params.buttonSize.toPx,
+                        params.buttonSize.toPx
+                )
+                setCompoundDrawablesWithIntrinsicBounds(
+                        null,
+                        context.getDrawable(entry.key),
+                        null,
+                        null)
+                compoundDrawablePadding = 8
+                setOnClickListener {
+                    if (params.collapseAfterButtonClick) isExpanded = false
+                    entry.value.invoke()
+                }
+                elevation = 0.0f
+                visibility = View.INVISIBLE
 
-            val view = Button(context)
-            view.id = View.generateViewId()
-            view.layoutParams = ConstraintLayout.LayoutParams(
-                    params.buttonSize.toPx,
-                    params.buttonSize.toPx
-            )
-            view.setCompoundDrawablesWithIntrinsicBounds(null, context.getDrawable(entry.key), null, null)
-            view.compoundDrawablePadding = 8
-            view.setOnClickListener { entry.value.invoke() }
-            view.setBackgroundResource(R.drawable.round_button)
-            view.clipToOutline
-            view.elevation = 0.0f
-            view.visibility = View.INVISIBLE
-            layout.addView(view, i++)
-            tint(view, ContextCompat.getColor(context, params.buttonsColor))
+                setBackgroundResource(R.drawable.round_button)
+                tint(this, ContextCompat.getColor(context, params.buttonaAvatarTintColor))
 
-            set.clone(layout)
-            set.connect(view.id, ConstraintSet.END, layout.id, ConstraintSet.END, params.buttonEndOffset.toPx
-            )
-            set.connect(view.id, ConstraintSet.BOTTOM, layout.id, ConstraintSet.BOTTOM, params.buttonBottomOffset.toPx
-            )
-            set.applyTo(layout)
+                val sd = background.mutate() as GradientDrawable
+                sd.setColor(ContextCompat.getColor(context, params.buttonBackgroundColor))
+                sd.invalidateSelf()
 
-            buttons.add(view)
+                layout.addView(this, i++)
+                with(set) {
+                    clone(layout)
+                    connect(id, ConstraintSet.END, layout.id, ConstraintSet.END, params.buttonEndOffset.toPx)
+                    connect(id, ConstraintSet.BOTTOM, layout.id, ConstraintSet.BOTTOM, params.buttonBottomOffset.toPx)
+                    applyTo(layout)
+                }
+                buttons.add(this)
+            }
         }
     }
 
-    fun tint(button: Button, @ColorInt color: Int) {
+    private fun tint(button: Button, @ColorInt color: Int) {
         val drawables = button.compoundDrawables
         for (drawable in drawables) {
             drawable?.setColorFilter(color, PorterDuff.Mode.SRC_IN)
@@ -153,14 +183,19 @@ class CustomFab @JvmOverloads constructor(
     }
 }
 
-val Int.toPx: Int
+private fun Int.removeBarHeight(horizontal: Boolean, h: Int): Int {
+    return if (horizontal) minus(h) else plus(h)
+}
+
+private val Int.toPx: Int
     get() = (this * Resources.getSystem().displayMetrics.density).toInt()
 
 data class FabParams(
         val buttonSize: Int = 50,
-        @ColorRes val buttonsColor: Int = R.color.accent_material_light,
+        @ColorRes val buttonaAvatarTintColor: Int = R.color.accent_material_light,
+        @ColorRes val buttonBackgroundColor: Int = R.color.primary_dark_material_light,
         val buttonBottomOffset: Int = 16,
-        val buttonEndOffset: Int = 16
-
+        val buttonEndOffset: Int = 16,
+        val collapseAfterButtonClick: Boolean = true,
+        val extractionOffset: Int = buttonEndOffset.times(4)
 )
-
